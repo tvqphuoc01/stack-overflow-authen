8e6a581bdedf1e40b619760e984388c16a03acd7
phuoc.tran <phuoc.tran@parcelperform.com>
2023-06-15 23:26:25 +0700

    create user api

---
+++ api/serializers/user_serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+class UserSerializer(serializers.Serializer):
+    full_name = serializers.CharField(required=True, max_length=100)
+    email = serializers.EmailField(required=True, max_length=100)
+    password = serializers.CharField(required=True, max_length=100)
---
+++ api/views/create_user.py
@@ -0,0 +1,62 @@
+from django_cryptography.fields import encrypt
+
+from api.models import User
+from api.serializers.user_serializers import UserSerializer
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['POST'])
+def create_new_user(request):
+    """
+        Create new user
+        :params request: 
+            - full_name
+            - email
+            - password
+            
+        :return:
+            201 - User created
+            400 - Bad request
+            500 - Internal server error
+    """
+    
+    # check request data is valid
+    serializer = UserSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    validated_data = serializer.validated_data
+    
+    # user info
+    name = validated_data.get('full_name')
+    email = validated_data.get('email')
+    password = validated_data.get('password')
+    
+    hashed_password = encrypt(password)
+    
+    # create new user
+    try:
+        # check if email already taken or not
+        # if not, create new user
+        created = User.objects.get_or_create(full_name=name, email=email, password=hashed_password)
+        if created:
+            return Response(
+                {
+                    'message': 'Email already taken'
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        else:
+            return Response(
+                {
+                    'message': 'User created'
+                },
+                status=status.HTTP_201_CREATED
+            )
+    except Exception as e:
+        return Response(
+            {
+                'message': 'Internal server error'
+            },
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
